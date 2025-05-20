5a35f25849fa8a74092da2ae92769a8ed7c478bd
A-N-I-T-T-A <133679742+A-N-I-T-T-A@users.noreply.github.com>
2025-05-20 18:10:28 +0530

    Create 0010_create_superuser.py

---
+++ mentorapp/migrations/0010_create_superuser.py
@@ -0,0 +1,23 @@
+from django.db import migrations
+from django.contrib.auth.hashers import make_password
+
+def create_superuser(apps, schema_editor):
+    User = apps.get_model('auth', 'User')
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create(
+            username='admin',
+            password=make_password('admin'),
+            is_superuser=True,
+            is_staff=True,
+            email='admin@gmail.com'
+        )
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0009_update_payment_fields'),  # Update to match your latest
+    ]
+
+    operations = [
+        migrations.RunPython(create_superuser),
+    ]
